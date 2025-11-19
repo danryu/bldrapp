b47279e242a8880884cf27c40f2b1ce849f20666
danryu <dan.garton@gmail.com>
2025-11-19 11:45:50 +0200

    add videorate to gstreamer build for now

--- build_gstreamer.sh
+++ build_gstreamer.sh
@@ -304,6 +304,7 @@ meson setup builddir \
 	-Dgst-plugins-base:gl=enabled \
 	-Dgst-plugins-base:playback=enabled \
 	-Dgst-plugins-base:app=enabled \
+	-Dgst-plugins-base:videorate=enabled \
 	-Dgst-plugins-base:videoconvertscale=enabled \
 	-Dgst-plugins-base:videotestsrc=enabled \
 	-Dgst-plugins-base:audioresample=enabled \
